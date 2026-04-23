6d116c8a5d289e24d165fe0b5b13f33174a249b6
richi-0-7 <dppss96148@gmail.com>
2026-04-23 21:52:34 +0800

    Update 0.test.php

--- 0.test.php
+++ 0.test.php
@@ -1,3 +1,21 @@
 <?php 
 phpinfo();
+
+/*
+php語法開頭需加：<?php
+結尾需加：?>
+
+phpinfo()：此指令執行可看到藍色表格，會有php版本資訊等...
+
+結尾：php每段程式用;作為結尾
+
+執行方法：右鍵/sever project/open file in browser
+
+<!--html註解-->
+*/
+
+/*php註解*/
+
 ?>
+
+
